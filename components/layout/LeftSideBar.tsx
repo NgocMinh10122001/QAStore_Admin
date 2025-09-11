5a9d7530385aa8d13a9dabcca9cb36b2a2b43428
Phạm Ngọc <phamngocminh10122001@gmail.com>
2025-09-11 17:23:46 +0700

    add auth sign-in/sign-up to app

---
+++ components/layout/LeftSideBar.tsx
@@ -0,0 +1,25 @@
+import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { navLinks } from '@/lib/constants'
+
+
+const LeftSideBar = () => {
+  return (
+    <div className='h-screen top-0 left-0 sticky p-10 flex flex-col gap-16 bg-blue-2  shadow-xl max-lg:hidden'>
+      <Image src={""} alt='logo' width={150} height={70}/>
+      <div className='flex flex-col gap-12'>
+        {navLinks.map((link) => {
+            return <Link key={link.label} href={link.url} className='flex gap-4 text-body-medium items-center'>{link.icon}<p>{link.label}</p></Link>
+        })}
+      </div>
+      <div className='flex gap-4 text-body-medium items-center'>
+        <UserButton/>
+        <p>Edit Profile</p>
+      </div>
+    </div>
+  )
+}
+
+export default LeftSideBar
